ed4cad22a011f689465ab25b145554db7c8dd7ba
figengungor <figengungor1@gmail.com>
2018-04-13 12:07:24 +0300

    Add click pending intent to widget

--- app/src/main/java/com/example/android/mygarden/widget/PlantWidgetProvider.java
+++ app/src/main/java/com/example/android/mygarden/widget/PlantWidgetProvider.java
@@ -1,11 +1,17 @@
-package com.example.android.mygarden;
+package com.example.android.mygarden.widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
+import com.example.android.mygarden.R;
+import com.example.android.mygarden.ui.MainActivity;
+
 //https://developer.android.com/guide/topics/appwidgets/index.html
+//https://developer.android.com/reference/android/widget/RemoteViews.html
 
 /**
  * Implementation of App Widget functionality.
@@ -15,15 +21,28 @@ public class PlantWidgetProvider extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.plant_widget_provider);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.plant_widget);
+
+        //PendingIntent is just a wrap around an intent that allows other applications to have
+        // access and run that intent in your application
+        //Create an Intent to launch MainActivity when clicked
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        //Widgets allow click handlers to only launch pending intents
+        views.setOnClickPendingIntent(R.id.grass_image, pendingIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
+    /*onUpdate is called once a new widget is created
+    * and every update interval which we set 30 mins in plant_widget_info.xml
+    *
+    * AppWidgetManager class gives access to information about all existing widgets
+    * on the home screen. It's also your access to forcing an update on all these widgets.
+    * */
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
