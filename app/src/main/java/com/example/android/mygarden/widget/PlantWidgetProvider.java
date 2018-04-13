170f515d8ede1509cab6389322540c34d459b856
figengungor <figengungor1@gmail.com>
2018-04-13 12:38:10 +0300

    Add click pending intent to water drop image Create PendingIntent to start PlantWateringService Water all plants and update last_watered timestamp

--- app/src/main/java/com/example/android/mygarden/widget/PlantWidgetProvider.java
+++ app/src/main/java/com/example/android/mygarden/widget/PlantWidgetProvider.java
@@ -33,6 +33,17 @@ public class PlantWidgetProvider extends AppWidgetProvider {
         //Widgets allow click handlers to only launch pending intents
         views.setOnClickPendingIntent(R.id.grass_image, pendingIntent);
 
+
+        // Add the wateringservice click handler
+        Intent wateringIntent = new Intent(context, PlantWateringService.class);
+        wateringIntent.setAction(PlantWateringService.ACTION_WATER_PLANTS);
+        PendingIntent wateringPendingIntent = PendingIntent.getService(
+                context,
+                0,
+                wateringIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        views.setOnClickPendingIntent(R.id.widget_water_button, wateringPendingIntent);
+
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
